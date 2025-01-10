00a7a0c45206edd8f050d38aff469962a5cd3838
ikhdaaakmalia <punyaamelaja@gmail.com>
2025-01-10 17:15:07 +0700

    membuat data class InsertUiState & FormErrorState

--- app/src/main/java/com/ikhdaamel/p10/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/ikhdaamel/p10/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,24 @@ package com.ikhdaamel.p10.ui.viewmodel
 
 import com.ikhdaamel.p10.model.Mahasiswa
 
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class FormErrorState(
+    val nim : String? =null,
+    val nama :String? =null,
+    val jenisKelamin : String? =null,
+    val alamat : String? =null,
+    val kelas : String? =null,
+    val angkatan : String? =null
+){
+    fun isValid(): Boolean{
+        return nim == null && nama == null && jenisKelamin == null && alamat == null && kelas == null && angkatan == null
+    }
+}
+
 //data class variabel yg menyimpan data input form
 data class MahasiswaEvent(
     val nim : String ="",
